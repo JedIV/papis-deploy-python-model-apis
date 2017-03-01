de260b90cab7d14ad1b6d23cb51e2fe2f760125a
Jed Dougherty <jed.dougherty@dataiku.com>
2017-03-01 12:15:38 -0800

    updated api to include shutdown function

--- api.py
+++ api.py
@@ -4,6 +4,12 @@ import joblib
 model = joblib.load('model/model.pkl')
 app = Flask("api")
 
+def shutdown_server():
+    func = request.environ.get('werkzeug.server.shutdown')
+    if func is None:
+        raise RuntimeError('Not running with the Werkzeug Server')
+    func()
+
 @app.route('/predict', methods=['POST'])
 def predict_api():
     """Endpoint for predicting truthfulness of a review."""
@@ -14,3 +20,8 @@ def predict_api():
 @app.route('/')
 def test():
     return jsonify({"status":"success"})
+
+@app.route('/shutdown', methods=['POST'])
+def shutdown():
+    shutdown_server()
+    return 'Server shutting down...'
